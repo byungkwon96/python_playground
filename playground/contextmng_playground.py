840fe3da546d09ddae27f8b1bbeec2e876271907
Byung kwon Kim <byungkwon96@gmail.com>
2022-07-01 14:40:42 +0900

    done playground

---
+++ playground/contextmng_playground.py
@@ -0,0 +1,64 @@
+with open("notes.txt", "w") as file:
+    file.write("some todooo...")
+
+"""
+file = open('notes.txt','w')
+try:
+    file.write('some todoo')
+finally:
+    file.close()
+"""
+
+from threading import Lock
+
+from regex import F
+
+lock = Lock()
+
+"""
+lock.acquire()
+lock.release()
+"""
+with lock:
+    print("do smt")
+
+# own classes
+class ManagedFile:
+    def __init__(self, filename):
+        print("init")
+        self.filename = filename
+
+    # need __enter__ method and __exit__ needs in order to use with...
+    def __enter__(self):
+        print("enter")
+        self.file = open(self.filename, "w")
+        return self.file
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        if self.file:
+            self.file.close()
+        if exc_type is not None:
+            print("exception has been handled")
+        print("exit")
+        return True
+
+
+with ManagedFile("notes.txt") as file:
+    print("do some thing...")
+    file.write("some toodoos")
+
+# implement as function
+from contextlib import contextmanager
+
+
+@contextmanager
+def open_managed_file(filename):
+    f = open(filename, "w")
+    try:
+        yield f
+    finally:
+        f.close()
+
+
+with open_managed_file("notes.txt") as f:
+    f.write("write smt...")
